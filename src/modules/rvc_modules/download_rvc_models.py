f550ce097c043bf55fb2d06a0840030ba816fb06
HWcoms <dlgusdn383@gmail.com>
2023-08-07 14:00:41 +0900

    models dl (file check) + model folders gitignore

--- src/modules/rvc_modules/download_rvc_models.py
+++ src/modules/rvc_modules/download_rvc_models.py
@@ -8,22 +8,30 @@ RVC_DOWNLOAD_LINK = 'https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 BASE_DIR = os.path.join(BASE_DIR, 'Models', 'rvc_model')
 
+logging = True
+
 # CHECK BASE_DIR
 if not os.path.exists(BASE_DIR):
     os.makedirs(BASE_DIR)
     print("\033[34m" + f"[download_rvc_models]: Created Base folder! \033[33m[{BASE_DIR}]" + "\033[0m")
 
-print(BASE_DIR)
-exit()
 
-mdxnet_models_dir = BASE_DIR / 'mdxnet_models'
-rvc_models_dir = BASE_DIR / 'rvc_models'
+def check_files(model_name, dir_name):
+    full_path = os.path.join(dir_name, model_name)
+    if os.path.exists(full_path):
+        return True
+    return False
+
+
+def mod_print(str_to_print):
+    if logging:
+        print(str_to_print)
 
 
 def dl_model(link, model_name, dir_name):
     with requests.get(f'{link}{model_name}') as r:
         r.raise_for_status()
-        with open(dir_name / model_name, 'wb') as f:
+        with open(os.path.join(dir_name, model_name), 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
 
@@ -31,12 +39,18 @@ def dl_model(link, model_name, dir_name):
 if __name__ == '__main__':
     mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx', 'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx']
     for model in mdx_model_names:
-        print(f'Downloading {model}...')
-        dl_model(MDX_DOWNLOAD_LINK, model, mdxnet_models_dir)
+        if not check_files(model, BASE_DIR):
+            mod_print(f'Downloading {model}...')
+            dl_model(MDX_DOWNLOAD_LINK, model, BASE_DIR)
+        else:
+            mod_print(f'{model} Found...')
 
     rvc_model_names = ['hubert_base.pt', 'rmvpe.pt']
     for model in rvc_model_names:
-        print(f'Downloading {model}...')
-        dl_model(RVC_DOWNLOAD_LINK, model, rvc_models_dir)
+        if not check_files(model, BASE_DIR):
+            mod_print(f'Downloading {model}...')
+            dl_model(RVC_DOWNLOAD_LINK, model, BASE_DIR)
+        else:
+            mod_print(f'{model} Found...')
 
-    print('All models downloaded!')
+    mod_print('All RVC required models are downloaded!')
